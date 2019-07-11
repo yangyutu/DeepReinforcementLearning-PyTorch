d22c9159dbfe57cb9a068a8fae6e119657e520b3
Yuguang Yang <yangyutu123@gmail.com>
2019-07-10 23:14:22 -0400

    update

--- Env/CustomEnv/TimedMaze/DQNExample/DQN_CNNDynMazeStochAgentGPU.py
+++ Env/CustomEnv/TimedMaze/DQNExample/DQN_CNNDynMazeStochAgentGPU.py
@@ -1,6 +1,7 @@
 
+
 from Agents.DQN.DQN import DQNAgent
-from Env.CustomEnv.DynamicMaze.DynamicMaze import DynamicMaze, TrajRecorder
+from Env.CustomEnv.DynamicMaze.DynamicMaze import DynamicMazeMultiMap, TrajRecorder
 from utils.netInit import xavier_init
 from Agents.DQN.DQNA3C import DQNA3CMaster, SharedAdam
 import json
@@ -18,15 +19,15 @@ import torch.nn.functional as F
 torch.set_num_threads(1)
 
 configName = 'config.json'
-with open(configName ,'r') as f:
+with open(configName,'r') as f:
     config = json.load(f)
-
+    
 # Convolutional neural network (two convolutional layers)
 class MulChanConvNet(nn.Module):
     def __init__(self, inputWidth, num_hidden, num_action):
         super(MulChanConvNet, self).__init__()
-
-        self.inputShape = (inputWidth ,inputWidth)
+        
+        self.inputShape = (inputWidth,inputWidth)
         self.layer1 = nn.Sequential( # input shape (1, inputWdith, inputWdith)
             nn.Conv2d(1,             # input channel
                       32,            # output channel
@@ -43,8 +44,8 @@ class MulChanConvNet(nn.Module):
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2)) # inputWdith / 2
         # add a fully connected layer
-        # width = int(inputWidth / 4) + 1
-
+        #width = int(inputWidth / 4) + 1
+        
         self.fc0 = nn.Linear(2, 32)
         self.fc1 = nn.Linear(self.featureSize() + 32, num_hidden)
         self.fc2 = nn.Linear(num_hidden, num_action)
@@ -57,7 +58,7 @@ class MulChanConvNet(nn.Module):
         xout = self.layer2(xout)
         xout = xout.reshape(xout.size(0), -1)
         # mask xout for test
-        # xout.fill_(0)
+        #xout.fill_(0)
         yout = F.relu(self.fc0(y))
         out = torch.cat((xout, yout), 1)
         out = F.relu(self.fc1(out))
@@ -70,7 +71,7 @@ class MulChanConvNet(nn.Module):
 
 def plotPolicy(policy, nbActions):
     idx, idy = np.where(policy >=0)
-    action = policy[idx ,idy]
+    action = policy[idx,idy]
     plt.scatter(idx, idy, c = action, marker='s', s = 10)
     # for i in range(nbActions):
     #     idx, idy = np.where(policy == i)
@@ -91,10 +92,21 @@ def stateProcessor(state, device = 'cpu'):
     senorList = [item['sensor'] for item in state if item is not None]
     targetList = [item['target'] for item in state if item is not None]
     nonFinalState = {'sensor': torch.tensor(senorList, dtype=torch.float32, device=device),
-                     'target': torch.tensor(targetList, dtype=torch.float32, device=device)}
+              'target': torch.tensor(targetList, dtype=torch.float32, device=device)}
     return nonFinalState, nonFinalMask
 
-env = DynamicMaze(config)
+def experienceProcessor(state, action, nextState, reward, info):
+    if nextState is not None:
+        target = info['previousTarget']
+        distance = target - info['currentState'][:2]
+        phi = info['currentState'][2]
+        dx = distance[0] * math.cos(phi) + distance[1] * math.sin(phi)
+        dy = -distance[0] * math.sin(phi) + distance[1] * math.cos(phi)
+        nextState['target'] = np.array([dx / info['scaleFactor'], dy / info['scaleFactor']])
+    return state, action , nextState, reward
+
+
+env = DynamicMazeMultiMap(config)
 
 N_S = env.stateDim[0]
 N_A = env.nbActions
@@ -106,7 +118,7 @@ optimizer = optim.Adam(policyNet.parameters(), lr=config['learningRate'])
 
 
 agent = DQNAgent(config, policyNet, targetNet, env, optimizer, torch.nn.MSELoss(reduction='none'), N_A,
-                 stateProcessor=stateProcessor)
+                 stateProcessor=stateProcessor, experienceProcessor=experienceProcessor)
 
 
 
@@ -119,38 +131,45 @@ if trainFlag:
         checkpoint = torch.load(config['saveModelFile'])
         agent.policyNet.load_state_dict(checkpoint['model_state_dict'])
         agent.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+    if config['loadCheckpointFlag']:
+        agent.load_checkpoint(config['loadCheckpointPrefix'])
 
 
     plotPolicyFlag = True
     if plotPolicyFlag:
 
         for phiIdx in range(8):
-            phi = phiIdx * np.pi /4.0
+            phi = phiIdx * np.pi/4.0
             policy = deepcopy(env.mapMat).astype(np.long)
             value = deepcopy(env.mapMat)
             for i in range(policy.shape[0]):
-                for j in range(policy.shape[1]):
-                    if env.mapMat[i, j] == 1:
-                        policy[i, j] = -1
-                        value[i, j] = -1
-                    else:
-                        sensorInfo = env.agent.getSensorInfoFromPos(np.array([i, j, phi]))
-                        distance = np.array(config['targetState']) - np.array([i, j])
-                        dx = distance[0] * math.cos(phi) + distance[1] * math.sin(phi)
-                        dy = distance[0] * math.sin(phi) - distance[1] * math.cos(phi)
-                        dx = agent.env.agent.targetClipMap(dx) if dx > 0 else -agent.env.agent.targetClipMap(-dx)
-                        dy = agent.env.agent.targetClipMap(dy) if dy > 0 else -agent.env.agent.targetClipMap(-dy)
-                        state = {'sensor': sensorInfo, 'target': np.array([dx, dy])}
-                        policy[i, j] = agent.getPolicy(state)
-                        Qvalue = agent.policyNet(stateProcessor([state], config['device'])[0])
-                        value[i, j] = Qvalue[0, policy[i ,j]].cpu().item()
-            np.savetxt('DynamicMazePolicyBeforeTrain' + config['mapName'] +'phiIdx' + str(phiIdx) + '.txt', policy,
-                       fmt='%d', delimiter='\t')
-            np.savetxt('DynamicMazeValueBeforeTrain' + config['mapName'] + 'phiIdx' + str(phiIdx) + '.txt', value,
-                       fmt='%.3f', delimiter='\t')
+                  for j in range(policy.shape[1]):
+                      if env.mapMat[i, j] == 1:
+                          policy[i, j] = -1
+                          value[i, j] = -1
+                      else:
+                          sensorInfo = env.agent.getSensorInfoFromPos(np.array([i, j, phi]))
+                          distance = np.array(config['targetState']) - np.array([i, j])
+                          dx = distance[0] * math.cos(phi) + distance[1] * math.sin(phi)
+                          dy = distance[0] * math.sin(phi) - distance[1] * math.cos(phi)
+                          if math.sqrt(dx ** 2 + dy ** 2) > agent.env.agent.targetClipLength:
+                              angle = math.atan2(dy, dx)
+                              dx = agent.env.agent.targetClipLength * math.cos(angle)
+                              dy = agent.env.agent.targetClipLength * math.sin(angle)
+                          #dx = agent.env.agent.targetClipMap(dx) if dx > 0 else -agent.env.agent.targetClipMap(-dx)
+                          #dy = agent.env.agent.targetClipMap(dy) if dy > 0 else -agent.env.agent.targetClipMap(-dy)
+                          state = {'sensor': sensorInfo, 'target': np.array([dx, dy])}
+                          policy[i, j] = agent.getPolicy(state)
+                          Qvalue = agent.policyNet(stateProcessor([state], config['device'])[0])
+                          value[i, j] = Qvalue[0, policy[i,j]].cpu().item()
+            np.savetxt('DynamicMazePolicyBeforeTrain' + config['mapName'] +'phiIdx'+ str(phiIdx) + '.txt', policy, fmt='%d', delimiter='\t')
+            np.savetxt('DynamicMazeValueBeforeTrain' + config['mapName'] + 'phiIdx' + str(phiIdx) + '.txt', value, fmt='%.3f',delimiter='\t')
     # plotPolicy(policy, N_A)
 
+
+
     agent.train()
+
 
     if plotPolicyFlag:
         for phiIdx in range(8):
@@ -167,22 +186,26 @@ if trainFlag:
                         distance = np.array(config['targetState']) - np.array([i, j])
                         dx = distance[0] * math.cos(phi) + distance[1] * math.sin(phi)
                         dy = distance[0] * math.sin(phi) - distance[1] * math.cos(phi)
-                        dx = agent.env.agent.targetClipMap(dx) if dx > 0 else -agent.env.agent.targetClipMap(-dx)
-                        dy = agent.env.agent.targetClipMap(dy) if dy > 0 else -agent.env.agent.targetClipMap(-dy)
+                        angle = math.atan2(dy, dx)
+                        if math.sqrt(dx ** 2 + dy ** 2) > agent.env.agent.targetClipLength:
+                            dx = agent.env.agent.targetClipLength * math.cos(angle)
+                            dy = agent.env.agent.targetClipLength * math.sin(angle)
+                        #dx = agent.env.agent.targetClipMap(dx) if dx > 0 else -agent.env.agent.targetClipMap(-dx)
+                        #dy = agent.env.agent.targetClipMap(dy) if dy > 0 else -agent.env.agent.targetClipMap(-dy)
                         state = {'sensor': sensorInfo, 'target': np.array([dx, dy])}
                         policy[i, j] = agent.getPolicy(state)
                         Qvalue = agent.policyNet(stateProcessor([state], config['device'])[0])
-                        value[i, j] = Qvalue[0, policy[i, j]].cpu().item()
-            np.savetxt('DynamicMazePolicyAfterTrain' + config['mapName'] + 'phiIdx' + str(phiIdx) + '.txt', policy,
-                       fmt='%d',
+                        value[i, j] = Qvalue[0, policy[i,j]].cpu().item()
+            np.savetxt('DynamicMazePolicyAfterTrain' + config['mapName'] + 'phiIdx' + str(phiIdx) + '.txt', policy, fmt='%d',
                        delimiter='\t')
             np.savetxt('DynamicMazeValueAfterTrain' + config['mapName'] + 'phiIdx' + str(phiIdx) + '.txt', value,
                        fmt='%.3f', delimiter='\t')
 
     torch.save({
-        'model_state_dict': agent.policyNet.state_dict(),
-        'optimizer_state_dict': agent.optimizer.state_dict(),
-    }, config['saveModelFile'])
+                'model_state_dict': agent.policyNet.state_dict(),
+                'optimizer_state_dict': agent.optimizer.state_dict(),
+                }, config['saveModelFile'])
+
 
 if testFlag:
     config['loadExistingModel'] = True
@@ -193,7 +216,8 @@ if testFlag:
         agent.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
 
     recorder = TrajRecorder()
+    agent.env.agent.config['stochMoveFlag'] = True
     agent.testPolicyNet(100, recorder)
     recorder.write_to_file(config['mapName'] + 'TestTraj.txt')
 
-# plotPolicy(policy, N_A)
+#plotPolicy(policy, N_A)
