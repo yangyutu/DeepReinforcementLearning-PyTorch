03a9f0f26b0e1475bcc7471282eb58cbb5930fc2
Yuguang Yang <yangyutu123@gmail.com>
2019-11-03 11:47:28 -0500

    update path search.

--- Env/CustomEnv/ThreeDNavigation/NavigationExamples/optimalSearch/getSearchPath.m
+++ Env/CustomEnv/ThreeDNavigation/NavigationExamples/optimalSearch/getSearchPath.m
@@ -33,7 +33,7 @@ pbaspect([1, 1, 1]);
 
 figure(3)
 k = 5;
-t = 0:0.04:100;
+t = 0:0.001:40;
 
 z = k*t;
 r = cos(t * 4) * 45;
@@ -42,12 +42,26 @@ r = cos(t * 4) * 45;
 x = r .* cos(t * 6);
 y = r .* sin(t * 6);
 
-
-scatter3(x, y, z,[], z, 'filled');
+colormap(jet)
+scatter3(x, y, z, 2, z, 'filled');
 hold on
-plot3(x, y, z);
-
+%plot3(x, y, z);
+plotCylinderFunc(50, 200)
 pbaspect([1, 1, 1]);
+
+xlim([-50 50])
+ylim([-50 50])
+zlim([0 200])
+%set(gca,'box','off')
+%set(gca,'visible','off')
+set(gca,'linewidth',2,'fontsize',16,'fontweight','bold','plotboxaspectratiomode','manual','xminortick','on','yminortick','on');
+set(gca,'TickLength',[0.04;0.02]);
+pbaspect([1 1 1])
+%saveas(gcf,'traj.png')
+axis off
+print(gcf,'traj.png','-dpng','-r600');
+
+
 
 figure(4)
 k = 5;
